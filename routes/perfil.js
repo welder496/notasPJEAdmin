029d7983079e924f17da181aa8cd624c2e190613
welder496 <welder496@gmail.com>
2015-09-08 10:41:33 -0300

    Adição de funções de gerenciamento de Perfil!!

--- routes/perfil.js
+++ routes/perfil.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var notasRest = require('notasrest');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('perfil');
+   notasRest.getPerfils(function(data){
+      res.render('perfil',{perfils: data});
+   });
 });
 
 router.post('/', function(req, res, next){
-  res.render('perfil');
+   notasRest.newPerfil(perfil,function(data){
+      var message="";
+      if (data.hasOwnProperty('message')) {
+            message = data.message;
+      }
+      notasRest.getPerfils(function(data){
+         res.render('perfil',{perfils: data, message: message, show: 'true'});
+      });
+   });
 });
 
 module.exports = router;
